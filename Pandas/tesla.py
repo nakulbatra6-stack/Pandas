b09dc57bea4bcfeac8e889704ab41c36f82d7c91
nakulbatra6-stack <nakulbatra6@gmail.com>
2026-03-28 14:02:12 +0530

    New Tesla

--- Pandas/tesla.py
+++ Pandas/tesla.py
@@ -2,13 +2,14 @@ import pandas as pd
 
 df = pd.read_csv("tesla.csv")
 
-print(df.info())
-print(df.isnull().sum())
+# print(df.info())
+# print(df.isnull().sum())
 
 
 # Date ❌ needs fixing
-df["Date"] = pd.to_datetime(df["Date"])
-print(df.info())
+# df["Date"] = pd.to_datetime(df["Date"])#default pandas assumes MM-DD-YYYY
+df["Date"] = pd.to_datetime(df["Date"], dayfirst=True)
+# print(df.info())
 
 df["Year"] = df["Date"].dt.year
 df["Month"] = df["Date"].dt.month
@@ -19,7 +20,19 @@ df["DayOfWeek"] = df["Date"].dt.dayofweek
 df = df.sort_values("Date")
 
 #duplicates
-print(df.duplicated().sum())
-
+# print(df.duplicated().sum())
 # If > 0:df.drop_duplicates(inplace=True)
 
+# scaling
+num_cols = ["Open", "High", "Low", "Close", "Adj Close", "Volume"]
+
+# normalization
+for col in num_cols:
+    print(col)
+    df[col + "_norm"] = (df[col] - df[col].min()) / (df[col].max() - df[col].min())
+
+# standardization
+for col in num_cols:
+    df[col + "_std"] = (df[col] - df[col].mean()) / df[col].std()
+
+print(df[["Open", "Open_norm", "Open_std"]].head())
